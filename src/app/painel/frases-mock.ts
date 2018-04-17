15b1f79f6a87422c9e4ee654711faf9d2743ce4e
Lucas Santos <lucasmas16@gmail.com>
2018-04-16 21:36:34 -0300

    Upload inicial de projeto

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model';
+
+export const FRASES: Frase[] = [
+    {fraseEng: 'Today is sunday', frasePtBr: 'Hoje é domingo'},
+    {fraseEng: 'I watch TV', frasePtBr: 'Eu assisto TV'},
+    {fraseEng: 'How are you?', frasePtBr: 'Como vai você?'},
+    {fraseEng: 'I eat bread', frasePtBr: 'Eu como pão'}
+];
---
+++ src/app/painel/painel.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, EventEmitter, Output, OnDestroy} from '@angular/core';
+import { Frase } from '../shared/frase.model';
+import { FRASES } from './frases-mock';
+
+@Component({
+  selector: 'app-painel',
+  templateUrl: './painel.component.html',
+  styleUrls: ['./painel.component.css']
+})
+export class PainelComponent implements OnInit, OnDestroy {
+
+  public frases: Frase[] = FRASES;
+  public instrucao = 'Traduza a frase:';
+  public resposta = '';
+  public rodada = 0;
+  public rodadaFrase: Frase;
+  public progresso = 0;
+  public tentativas = 3;
+
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter();
+
+  constructor() {
+    this.atualizaRodada();
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+  }
+  public atualizarResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+  }
+
+  public verificarResposta(): void {
+    if (this.rodadaFrase.frasePtBr === this.resposta) {
+
+      this.rodada++;
+
+      this.progresso += (100 / this.frases.length);
+
+      if (this.rodada === this.frases.length) {
+        this.encerrarJogo.emit('vitoria');
+      } else {
+        this.atualizaRodada();
+      }
+
+
+    } else {
+      this.tentativas--;
+      if (this.tentativas < 0) {
+        this.encerrarJogo.emit('derrota');
+      }
+    }
+  }
+  public atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada];
+    this.resposta = '';
+  }
+}
